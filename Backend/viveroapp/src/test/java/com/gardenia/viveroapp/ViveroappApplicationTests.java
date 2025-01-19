c1ba3062c28e439e8629087a15e3fc9b896fba2b
Adrianシロ <adrianstratos@gmail.com>
2025-01-19 07:54:39 -0500

    Directory init

---
+++ Backend/viveroapp/src/test/java/com/gardenia/viveroapp/ViveroappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gardenia.viveroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViveroappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

12ad91783c0fa0d64cd4fbf0b581bc7f9b98774d
Adrianシロ <adrianstratos@gmail.com>
2025-01-19 07:47:48 -0500

    backend base

---
+++ viveroapp/src/main/java/com/gardenia/viveroapp/ViveroappApplication.java
@@ -0,0 +1,13 @@
+package com.gardenia.viveroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViveroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViveroappApplication.class, args);
+	}
+
+}
